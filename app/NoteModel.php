cb96ec1dad7a0d807a834ce98241ca7f73958626
Nikita Glushkov <glushkov.nik1@gmail.com>
2021-02-06 12:52:24 +0300

    fix model

--- app/NoteModel.php
+++ app/NoteModel.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class NoteModel extends Model
 {
-    protected string $table = 'notes';
-    protected array $fillable = ['text'];
+    protected $table = 'notes';
+    protected $fillable = ['text'];
 }
